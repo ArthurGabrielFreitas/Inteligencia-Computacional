e0a27ade688229932c03b9ef010159a2337957cb
ArthurGabrielFreitas <artgabrielfdm@gmail.com>
2025-04-25 17:08:22 -0300

    Solução via IA para ordenação e seleção. Ajuste na lógica de gerar disciplinas.

--- script.js
+++ script.js
@@ -31,19 +31,9 @@ function main() {
         avaliacao: avaliacoes[index],
     }));
 
-    // console.log(populacaoAvaliada);
-
-    const populacaoOrdenada = ordenacaoMergeSort(populacaoAvaliada);
-
-    // console.log(populacaoOrdenada);
-
-    // const populacaoOrdenadaNativa = ordenacaoNativa(populacaoAvaliada);
-
-    // console.log(populacaoOrdenadaNativa);
+    const populacaoOrdenada = populacaoAvaliada.sort((a, b) => a.avaliacao - b.avaliacao);
 
     const populacaoSelecionada = selecao(populacaoOrdenada);
-
-    console.log(populacaoSelecionada);
 }
 
 function gerarPopulacaoInicial(
@@ -69,25 +59,23 @@ function gerarPopulacaoInicial(
     );
 }
 
-function gerarCodigosDisciplinas(professores, disciplinas, horariosPorDia) {
-    const codigos = [];
+function gerarCodigosDisciplinas(professores, disciplinas, horariosDia) {
+    const listCodigos = [];
+    let codDis = 0;
 
-    for (let disciplina = 0; disciplina < disciplinas; disciplina++) {
-        const codigoDisciplina = disciplina.toString().padStart(2, "0");
-
-        for (let professor = 0; professor < professores; professor++) {
-            const codigoProfessor = professor.toString().padStart(2, "0");
-
-            for (let horario = 0; horario < horariosPorDia; horario++) {
-                const codigoAula = USE_CLASS_NUMBER_IN_CODE
-                    ? `${horario}${codigoProfessor}${codigoDisciplina}`
-                    : `${codigoProfessor}${codigoDisciplina}`;
-                codigos.push(codigoAula);
+    while (codDis < disciplinas) {
+        for (let codProf = 0; codProf < professores && codDis < disciplinas; codProf++) {
+            for (let j = 0; j < horariosDia; j++) {
+                const aula = USE_CLASS_NUMBER_IN_CODE
+                    ? `${j}${codProf.toString().padStart(2, "0")}${codDis.toString().padStart(2, "0")}`
+                    : `${codProf.toString().padStart(2, "0")}${codDis.toString().padStart(2, "0")}`;
+                listCodigos.push(aula);
             }
+            codDis++;
         }
     }
 
-    return codigos;
+    return listCodigos;
 }
 
 function dividirCodigosPorSemestre(codigos, intervaloSemestre) {
@@ -105,17 +93,14 @@ function embaralharIndividuo(individuoBase) {
 }
 
 function embaralharSemestre(semestre) {
-    const semestreEmbaralhado = [...semestre];
-
-    for (let i = semestreEmbaralhado.length - 1; i > 0; i--) {
+    for (let i = semestre.length - 1; i > 0; i--) {
         const indiceAleatorio = Math.floor(Math.random() * (i + 1));
-        [semestreEmbaralhado[i], semestreEmbaralhado[indiceAleatorio]] = [
-            semestreEmbaralhado[indiceAleatorio],
-            semestreEmbaralhado[i],
+        [semestre[i], semestre[indiceAleatorio]] = [
+            semestre[indiceAleatorio],
+            semestre[i],
         ];
     }
-
-    return semestreEmbaralhado;
+    return semestre;
 }
 
 function avaliarPopulacao(populacao, intervaloSemestre, periodos) {
@@ -128,43 +113,16 @@ function avaliarIndividuo(individuo, intervaloSemestre, periodos) {
     let conflitos = 0;
 
     for (let i = 0; i < intervaloSemestre; i++) {
-        // Lógica para comparar as aulas de semestres seguintes (2 x 3, 2 x 4, etc)
-        for (
-            let j = 0;
-            j < (periodos - 1) * intervaloSemestre;
-            j += intervaloSemestre
-        ) {
-            let numeroPrimeiraAula = i + j;
-            let aula1 = individuo[numeroPrimeiraAula];
-            let professor1;
-            if (indicadorNumeroAula) {
-                professor1 = aula1.slice(1, 3);
-            } else {
-                professor1 = aula1.slice(0, 2);
-            }
-            // Lógica para definir as aulas dos semestres seguintes
-            for (
-                let k = j;
-                k < periodos * intervaloSemestre;
-                k += intervaloSemestre
-            ) {
-                if (k == j) {
-                    continue;
-                }
-                let numeroProximaAula = i + k;
-                const aula2 = individuo[numeroProximaAula];
-                let professor2;
-                if (indicadorNumeroAula) {
-                    professor2 = aula2.slice(1, 3);
-                } else {
-                    professor2 = aula2.slice(0, 2);
-                }
-                if (professor1 == professor2) {
-                    avaliacao++;
-                    // Mostra no console as aulas com mesmo professor para conferência manual
-                    // console.log(aula1 + " " + aula2);
-                    // console.log(i + 1, numeroProximaAula + 1);
-                    // console.log(avaliacao);
+        for (let j = 0; j < (periodos - 1) * intervaloSemestre; j += intervaloSemestre) {
+            const aula1 = individuo[i + j];
+            const professor1 = extrairCodigoProfessor(aula1);
+
+            for (let k = j + intervaloSemestre; k < periodos * intervaloSemestre; k += intervaloSemestre) {
+                const aula2 = individuo[i + k];
+                const professor2 = extrairCodigoProfessor(aula2);
+
+                if (professor1 === professor2) {
+                    conflitos++;
                 }
             }
         }
@@ -177,44 +135,11 @@ function extrairCodigoProfessor(aula) {
     return USE_CLASS_NUMBER_IN_CODE ? aula.slice(1, 3) : aula.slice(0, 2);
 }
 
-function ordenacaoMergeSort(populacao) {
-    if (populacao.length == 1) {
-        return populacao;
-    }
-    const metadeVetorInicial = Math.floor(populacao.length / 2);
-    const vetor1 = ordenacaoMergeSort(populacao.slice(0, metadeVetorInicial));
-    const vetor2 = ordenacaoMergeSort(populacao.slice(metadeVetorInicial));
-    const resultado = [];
-
-    while (vetor1.length > 0 && vetor2.length > 0) {
-        if (vetor1[0].avaliacao <= vetor2[0].avaliacao) {
-            resultado.push(vetor1.shift());
-        } else {
-            resultado.push(vetor2.shift());
-        }
-    }
-
-    return resultado.concat(vetor1).concat(vetor2);
-}
-
-function ordenacaoNativa(populacao) {
-    return populacao.sort((a, b) => a.avaliacao - b.avaliacao);
-}
-
 function selecao(populacaoOrdenada) {
-    const numeroAleatorio1 = Math.floor(
-        (Math.random() * populacaoOrdenada.length) / 2
-    );
-    const numeroAleatorio2 = Math.floor(
-        Math.random() * populacaoOrdenada.length
-    );
+    const indice1 = Math.floor((Math.random() * populacaoOrdenada.length) / 2);
+    const indice2 = Math.floor(Math.random() * populacaoOrdenada.length);
 
-    const populacaoSelecionada = [
-        populacaoOrdenada[numeroAleatorio1],
-        populacaoOrdenada[numeroAleatorio2],
-    ];
-
-    return populacaoSelecionada;
+    return [populacaoOrdenada[indice1], populacaoOrdenada[indice2]];
 }
 
 main();
